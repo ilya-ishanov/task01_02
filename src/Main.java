ff6bbf17cd5f6541680455910f5976584ecc833d
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2024-09-21 02:01:34 +0500

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Блядь не ужели");
+        System.out.println("вериться с трудом");
     }
 }
